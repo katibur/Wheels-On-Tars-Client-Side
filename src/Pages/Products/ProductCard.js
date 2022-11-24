bacb2a30e231b3379bca0ab4ef390b0c88ff6f5b
katibur <katiburrahmansany@gmail.com>
2022-11-24 21:51:26 +0600

    products shown successfully

--- src/Pages/Products/ProductCard.js
+++ src/Pages/Products/ProductCard.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
-    const { img, name } = product;
+    const { img, name, location, originalPrice, resalePrice, warranty, used, sellerName } = product;
+
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={img} alt={name} /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-body my-6">
+                <h2 className="card-title font-bold mx-auto mb-4">{name}</h2>
+                <p><span className='font-bold'>Place: </span>{location}</p>
+                <p><span className='font-bold'>Original Price:</span> {originalPrice}</p>
+                <p><span className='font-bold'>Resale Price:</span> {resalePrice}</p>
+                <p><span className='font-bold'>Warranty Left:</span> {warranty}</p>
+                <p><span className='font-bold'>Used For:</span> {used}</p>
+                <p><span className='font-bold'>Seller Name:</span> {sellerName}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">Book Now</button>
                 </div>
             </div>
         </div>
