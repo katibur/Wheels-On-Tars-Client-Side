5b40d688c35fc46aaaa575f624977a3ff3c77a24
katibur <katiburrahmansany@gmail.com>
2022-11-28 02:06:21 +0600

    all sellers and all buyers done

--- src/Pages/AllBuyers/AllBuyers.js
+++ src/Pages/AllBuyers/AllBuyers.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
 
 const AllBuyers = () => {
+
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users')
+            const data = await res.json();
+            return data;
+        }
+    });
+
     return (
         <div>
+            <h3 className='text-3xl'>All Users</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
 
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users.map((user, i) =>
+                                <tr key={user._id}>
+                                    {
+                                        user?.role === 'buyer' &&
+                                        <>
+                                            <th>{i + 1}</th>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.role}</td></>
+                                    }
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
